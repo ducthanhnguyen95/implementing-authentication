8f31e05e7e5953a2ffba115971ecb1588b2a9b19
thanhnd <ducthanhnguyen95@gmail.com>
2023-06-24 23:04:14 +0700

    Applying custom authentication logic

---
+++ src/test/java/com/example/implementing/authentication/ImplementingAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.implementing.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImplementingAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
